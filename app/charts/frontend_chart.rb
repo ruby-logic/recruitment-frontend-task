8f572f1e1e99c9ef3001794ed6fb4d0043493ac7
Alek Niemczyk <alek@rubylogic.pl>
2019-09-28 21:36:02 +0200

    FrontendChart

--- app/charts/frontend_chart.rb
+++ app/charts/frontend_chart.rb
@@ -1,21 +1,31 @@
 class FrontendChart
-  LOCATION_SYMBOLS = %w[
-    AB1
-    AB2
-    DD3
-    ER4
-    CD5
-  ].map(&:freeze).freeze
+  UnknownLocation = Class.new(StandardError)
+  DateRangeTooNarrow = Class.new(StandardError)
+  DateRangeTooWide = Class.new(StandardError)
+
+  LOCATION_SYMBOLS = %w[AB1 AB2 DD3 ER4 CD5].map(&:freeze).freeze
+  MINIMUM_DATE_RANGE = 7
+  MAXIMUM_DATE_RANGE = 100
 
   def initialize(params)
     @params = params
+    sanitize_params
   end
 
   def results
-    (1..12).map { |i| { date: Date.current + i.days, value: i * 100 } }
+    @dates.map do |date|
+      value = LOCATION_SYMBOLS.find_index(@location_symbol).to_i * 100 + date.yday % 81
+      { date: date, value: value }
+    end
   end
 
   private
 
-  def
+  def sanitize_params
+    @dates = (@params[:start_date].to_date..@params[:finish_date].to_date).to_a
+    @location_symbol = ([@params[:location_symbol]] & LOCATION_SYMBOLS).first
+    raise UnknownLocation if @location_symbol.nil?
+    raise DateRangeTooNarrow if @dates.size < MINIMUM_DATE_RANGE
+    raise DateRangeTooWide if @dates.size > MAXIMUM_DATE_RANGE
+  end
 end
--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,5 +1,23 @@
 class RecruitmentsController < ApplicationController
+  rescue_from FrontendChart::UnknownLocation, with: :unknown_location_error
+  rescue_from FrontendChart::DateRangeTooNarrow, with: :date_range_too_narrow_error
+  rescue_from FrontendChart::DateRangeTooWide, with: :date_range_too_wide_error
+
   def chart_data
     render json: { results: FrontendChart.new(params).results }
+  end
+
+  private
+
+  def unknown_location_error
+    render json: { error: 'Unknown location' }
+  end
+
+  def date_range_too_narrow_error
+    render json: { error: "Date range too narrow. Minimum #{FrontendChart::MINIMUM_DATE_RANGE} days" }
+  end
+
+  def date_range_too_wide_error
+    render json: { error: "Date range too wide. Muximum #{FrontendChart::MAXIMUM_DATE_RANGE} days" }
   end
 end
